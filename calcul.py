10774426db4a13a4eb879d1243cd3608bb6916ff
Yumeclock <yumeclock@gmail.com>
2026-03-06 15:16:46 +0100

    v2 : Ajout soustration, multiplication, division

--- calcul.py
+++ calcul.py
@@ -1,9 +1,22 @@
 # calcul.py - Calculatrice simple v1
 print("===Calculatrice Simple===")
+print("Opérations disponibles : +, -, *, /")
 print()
 
 a = float(input("Entrez le premier nombre : "))
+operation = input("Entrez l'opération(+, -, *, /) : ")
 b = float(input("Entrez le deuxième nombre : "))
 
-resulat = a + b
+if operation == "+":
+    resulat = a + b
+elif operation == "-":
+    resulat = a - b
+elif operation == "*":
+    resulat = a * b
+elif operation == "/":
+    resulat = a / b
+else:
+    print("Opération inconnue !")
+    exit()
+
 print(f"Résultat de l'addition : {a} + {b} = {resulat}")
