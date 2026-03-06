74456066e51c0dbe73687c2caeb66c59c6354079
Yumeclock <yumeclock@gmail.com>
2026-03-06 15:08:51 +0100

    v1 : calculatrice avec addition

---
+++ calcul.py
@@ -0,0 +1,9 @@
+# calcul.py - Calculatrice simple v1
+print("===Calculatrice Simple===")
+print()
+
+a = float(input("Entrez le premier nombre : "))
+b = float(input("Entrez le deuxième nombre : "))
+
+resulat = a + b
+print(f"Résultat de l'addition : {a} + {b} = {resulat}")
